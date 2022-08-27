6c614f825965949084e7e35d1d45d748f1cdac2c
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-27 11:47:53 +0300

    Securing Pages Client-side
    Securing Pages Client-side

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,90 +4,63 @@ import React from "react";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 
-//Şimdi session bilgilerini kullanmayı öğrenelim.
-//Öncelikle App js içine girip uygulamamızı sarmalayalım.
-
 export default function Navbar() {
-  //Sesiion kullararak bilgilere erişmeyi deneyelim.
   const { data: session, status } = useSession();
-  //Öncelikle console yazdırıp sonucu görelim.
-  /*
 
-{user: {…}, expires: '2022-09-26T06:51:26.592Z'}
-expires: "2022-09-26T06:51:26.592Z"
-user:
-email: "kubilaybozak@gmail.com"
-image: "https://avatars.githubusercontent.com/u/48950686?v=4"
-name: "Kubilay Bozak"
-[[Prototype]]: Object
-[[Prototype]]: Object
-
-konsol çıktısı bu şekilde bir obje dödürüyor şimdi bu obheyi kullanarak ekranda giriş yap kısmını modefiye edelim.
-
-  */
-
-  //Eğer giriş işlemi yapılırken sunucudan yanıt beklendğiği sırada bir loading göstermek istiyorsak
-  //Status durumunu kullanmamız gerekiyor eğer statur durumu loading ise butona loading yada ekrana loading dueumları gösdterbilriiz.
-
-  console.log(session);
-  if (status === "loading") {
-    return <h2>LOADİNG</h2>;
-  } else {
-    return (
-      <div className="w-full bg-black flex flex-row justify-between p-6">
-        <div className="text-white font-extrabold text-3xl">
-          <h1>
-            <a href="#">NextAUTH</a>
-          </h1>
-        </div>
-        <div className="flex flex-row gap-x-5 text-white">
-          <div className="flex flex-row w-38 items-center justify-center">
-            <Link href={"/"}>Home</Link>
-          </div>
-          <div className="flex flex-row w-38 items-center justify-center">
-            <Link href={"/dashboard"}>Dashboard</Link>
-          </div>
-          <div className="flex flex-row w-38 items-center justify-center">
-            <Link href={"/blog"}>Blog</Link>
-          </div>
-          {session ? (
-            <div className="flex flex-row w-38 items-center justify-center">
-              <img
-                src={session.user.image}
-                className=" rounded-full w-8 h-8 mr-4"
-              />
-              <span>{session.user.name}</span>
-            </div>
-          ) : (
-            <div className="flex flex-row w-38 items-center justify-center">
-              <Link href={"/api/auth/signout"}>
-                <a
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signIn();
-                  }}
-                >
-                  Sign In
-                </a>
-              </Link>
-            </div>
-          )}
-          {session ? (
-            <div className="flex flex-row w-38 items-center justify-center">
-              <Link href={"/api/auth/signout"}>
-                <a
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signOut();
-                  }}
-                >
-                  Sign Out
-                </a>
-              </Link>
-            </div>
-          ) : null}
-        </div>
+  return (
+    <div className="w-full bg-black flex flex-row justify-between p-6">
+      <div className="text-white font-extrabold text-3xl">
+        <h1>
+          <a href="/">NextAUTH</a>
+        </h1>
       </div>
-    );
-  }
+      <div className="flex flex-row gap-x-5 text-white">
+        <div className="flex flex-row w-38 items-center justify-center">
+          <Link href={"/"}>Home</Link>
+        </div>
+        <div className="flex flex-row w-38 items-center justify-center">
+          <Link href={"/dashboard"}>Dashboard</Link>
+        </div>
+        <div className="flex flex-row w-38 items-center justify-center">
+          <Link href={"/blog"}>Blog</Link>
+        </div>
+        {session ? (
+          <div className="flex flex-row w-38 items-center justify-center">
+            <img
+              src={session.user.image}
+              className=" rounded-full w-8 h-8 mr-4"
+            />
+            <span>{session.user.name}</span>
+          </div>
+        ) : (
+          <div className="flex flex-row w-38 items-center justify-center">
+            <Link href={"/api/auth/signout"}>
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                Sign In
+              </a>
+            </Link>
+          </div>
+        )}
+        {session ? (
+          <div className="flex flex-row w-38 items-center justify-center">
+            <Link href={"/api/auth/signout"}>
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign Out
+              </a>
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,55 @@
-import React from "react";
-import Navbar from 'components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "components/Navbar";
+
+import { getSession, signIn } from "next-auth/react";
+
+//Securing Pages Client-side
+/*
+Client  tarafından hiçbir kullanıcının giriş yapmadan buraya erişmesini istemiyoruz diyelim.
+Öncelikle 
+import { getSession} from "next-auth/react"; 
+kullanarak session bilgilerine erişelim.
+
+
+Eğer session 
+    varsa 
+          ekranda dashboard sayfası 
+    yoksa 
+          giriş yap kısmına yönlendirmek isteyelim.
+
+
+//GetSession bize ya session objesi yada null bir obje yolluyor.
+
+*/
+
 export default function Dashboard() {
+
+
+  const [loading,setLoading]=useState(false);
+
+
+  //Sadece comp. render edilirken bir kere çalışıyot componentDidMount()
+  
+  useEffect(() => {
+    const securePage=async()=>{
+
+//GetSession bize ya session objesi yada null bir obje yolluyor.
+      const session=await getSession();
+      if(session){
+
+      }
+      else{
+        signIn();
+      }
+    }  
+    securePage();
+  },[]);
+
+
   return (
     <>
       <Navbar />
-      <div>dashboard</div>
+      {getSession ? "var" : "yok"}
     </>
   );
 }
