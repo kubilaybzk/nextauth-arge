daecb44726a7bed9e7eb07138c1ff856fac9ab36
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-27 12:13:14 +0300

    Securing Pages Client-side(getSession)
    Securing Pages Client-side(getSession)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "components/Navbar";
-
-import { getSession, signIn } from "next-auth/react";
+import { getSession, signIn, useSession } from "next-auth/react";
+import Image from "next/image";
 
 //Securing Pages Client-side
 /*
@@ -23,33 +23,46 @@ Eğer session
 */
 
 export default function Dashboard() {
-
-
-  const [loading,setLoading]=useState(false);
-
-
+  const { data: userInfo, status } = useSession();
   //Sadece comp. render edilirken bir kere çalışıyot componentDidMount()
-  
   useEffect(() => {
-    const securePage=async()=>{
-
-//GetSession bize ya session objesi yada null bir obje yolluyor.
-      const session=await getSession();
-      if(session){
-
-      }
-      else{
+    const securePage = async () => {
+      //GetSession bize ya session objesi yada null bir obje yolluyor.
+      const session = await getSession();
+      if (!session) {
         signIn();
       }
-    }  
+    };
     securePage();
-  },[]);
-
+  }, []);
 
   return (
     <>
       <Navbar />
-      {getSession ? "var" : "yok"}
+      <div className="mx-auto w-full pt-20 px-8">
+        {userInfo ? (
+          <div className="flex flex-row justify-around">
+            <div className="w-64 h-64 relative">
+              <Image
+                src={userInfo.user.image}
+                objectFit="cover"
+                layout="fill"
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <div className="flex flex-row gap-x-6">
+                <h2>UserName:</h2>
+                <h2> {userInfo.user.name}</h2>
+              </div>
+              <div className="flex flex-row gap-x-6">
+                <h2>Email:</h2>
+                <h2> {userInfo.user.email}</h2>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </>
   );
 }
