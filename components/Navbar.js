39e8132118df7a5f7e999cbea81aab2d965c47d9
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-27 10:34:11 +0300

    Show logout button when it has session
    Show logout button when it has session

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from "react";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
@@ -7,7 +9,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Navbar() {
   //Sesiion kullararak bilgilere erişmeyi deneyelim.
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   //Öncelikle console yazdırıp sonucu görelim.
   /*
 
@@ -24,58 +26,68 @@ konsol çıktısı bu şekilde bir obje dödürüyor şimdi bu obheyi kullanarak
 
   */
 
-  return (
-    <div className="w-full bg-black flex flex-row justify-between p-6">
-      <div className="text-white font-extrabold text-3xl">
-        <h1>
-          <a href="#">NextAUTH</a>
-        </h1>
-      </div>
-      <div className="flex flex-row gap-x-5 text-white">
-        <div className="flex flex-row w-38 items-center justify-center">
-          <Link href={"/"}>Home</Link>
+  //Eğer giriş işlemi yapılırken sunucudan yanıt beklendğiği sırada bir loading göstermek istiyorsak
+  //Status durumunu kullanmamız gerekiyor eğer statur durumu loading ise butona loading yada ekrana loading dueumları gösdterbilriiz.
+
+  console.log(session);
+  if (status === "loading") {
+    return <h2>LOADİNG</h2>;
+  } else {
+    return (
+      <div className="w-full bg-black flex flex-row justify-between p-6">
+        <div className="text-white font-extrabold text-3xl">
+          <h1>
+            <a href="#">NextAUTH</a>
+          </h1>
         </div>
-        <div className="flex flex-row w-38 items-center justify-center">
-          <Link href={"/dashboard"}>Dashboard</Link>
-        </div>
-        <div className="flex flex-row w-38 items-center justify-center">
-          <Link href={"/blog"}>Blog</Link>
-        </div>
-        {session ? (
+        <div className="flex flex-row gap-x-5 text-white">
           <div className="flex flex-row w-38 items-center justify-center">
-            <img
-              src={session.user.image}
-              className=" rounded-full w-8 h-8 mr-4"
-            />
-            <span>{session.user.name}</span>
+            <Link href={"/"}>Home</Link>
           </div>
-        ) : (
           <div className="flex flex-row w-38 items-center justify-center">
-            <Link href={"/api/auth/signout"}>
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signIn();
-                }}
-              >
-                Sign In
-              </a>
-            </Link>
+            <Link href={"/dashboard"}>Dashboard</Link>
           </div>
-        )}
-        <div className="flex flex-row w-38 items-center justify-center">
-          <Link href={"/api/auth/signout"}>
-            <a
-              onClick={(e) => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              Sign Out
-            </a>
-          </Link>
+          <div className="flex flex-row w-38 items-center justify-center">
+            <Link href={"/blog"}>Blog</Link>
+          </div>
+          {session ? (
+            <div className="flex flex-row w-38 items-center justify-center">
+              <img
+                src={session.user.image}
+                className=" rounded-full w-8 h-8 mr-4"
+              />
+              <span>{session.user.name}</span>
+            </div>
+          ) : (
+            <div className="flex flex-row w-38 items-center justify-center">
+              <Link href={"/api/auth/signout"}>
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signIn();
+                  }}
+                >
+                  Sign In
+                </a>
+              </Link>
+            </div>
+          )}
+          {session ? (
+            <div className="flex flex-row w-38 items-center justify-center">
+              <Link href={"/api/auth/signout"}>
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </a>
+              </Link>
+            </div>
+          ) : null}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
