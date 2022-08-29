e2aa00cd14b8bc4ca90ae118dc1b3022b58e58b2
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-29 09:35:45 +0300

    Server-side Authentication
    Server-side Authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,37 @@
 import Navbar from 'components/Navbar'
+import { getSession, useSession } from 'next-auth/react'
 import React from 'react'
 
-export default function Blog() {
+
+//Server taraf
+
+
+
+
+export default function Blog(props) {
+  const {data}=useSession();
+
   return (
     <>
     <Navbar/>
-    <div>blog</div>
+
+      <h1 className='text-3xl'>{props.data}</h1>
+      <h2>Dataları server-side olarak alıyoruz.</h2>
     </>
   )
+}
+
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps = async (context) => {
+  const session=await getSession(context);
+  console.log(session)
+
+  return {
+    props: {
+        session,
+        data:session?"Üyeye özel bloglar":"halka açık bloglar"    
+    }
+  }
 }
