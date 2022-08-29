4855a2c1f77b2515784e4cee37d7190f25b4d228
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-29 10:57:07 +0300

    Securing Pages Server-side

--- pages/blog.js
+++ pages/blog.js
@@ -1,37 +1,55 @@
-import Navbar from 'components/Navbar'
-import { getSession, useSession } from 'next-auth/react'
-import React from 'react'
+import Navbar from "components/Navbar";
+import { getSession, useSession } from "next-auth/react";
+import React from "react";
 
-
-//Server taraf
-
-
-
+//Server tarafından eğer sesion yoksa giriş yap sayfasına return edip
+//Giriş yaptıktan sonra tekrar istenen sayfaya yönlendirme işlemi.
+//Önce session var mı yok mu kontrol edelim.
+//Daha sonra
+//Eğer session varsa
+//ssr işe sayfayı gösterelim
+//Eğer session yoksa
+//giriş yap kısmına yönlendirelim.
 
 export default function Blog(props) {
-  const {data}=useSession();
+  const { data } = useSession();
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-      <h1 className='text-3xl'>{props.data}</h1>
+      <h1 className="text-3xl">{props.data}</h1>
       <h2>Dataları server-side olarak alıyoruz.</h2>
     </>
-  )
+  );
 }
 
-
-// You should use getServerSideProps when:
-// - Only if you need to pre-render a page whose data must be fetched at request time
 export const getServerSideProps = async (context) => {
-  const session=await getSession(context);
-  console.log(session)
+  const session = await getSession(context);
+
+  //Session yoksa yani kullanıcı giriş yapmamış ise.
+
+  if (!session) {
+    //kullanıcı girişi yok:
+    return {
+      redirect: {
+        //iki adet key alıyor
+        //destination çalışcak api diyebiliriz.
+        /*
+permanent true or false 
+true ise istemcilere/arama motorlarına yönlendirmeyi sonsuza kadar önbelleğe alma talimatı veren 308 durum kodunu kullanır, 
+false ise geçici olan ve önbelleğe alınmamış 307 durum kodunu kullanır.
+        */
+        destination: `api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanet: false,
+      },
+    };
+  }
 
   return {
     props: {
-        session,
-        data:session?"Üyeye özel bloglar":"halka açık bloglar"    
-    }
-  }
-}
+      session,
+      data: session ? "Üyeye özel bloglar" : "halka açık bloglar",
+    },
+  };
+};
